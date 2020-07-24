5a6945e22541ebfb9458fb4e97acb3dbaeb40b87
Aleksandr Zelenskiy <dezion777@gmail.com>
2020-07-24 15:36:14 +0300

    add put request change hero name

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -11,19 +10,20 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
 
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
       this.log(`${operation} failed: ${error.message}`);
 
       // Let the app keep running by returning an empty result.
@@ -31,13 +31,16 @@ export class HeroService {
     };
   }
 
-  private heroesUrl = 'api/heroes';  // URL to web api
 
-  constructor(
-    private messageService: MessageService,
-    private http: HttpClient) { }
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
 
-  /** GET heroes from the server */
+  /** GET hero by id. Will 404 if id not found */
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
@@ -51,7 +54,6 @@ export class HeroService {
   }
 
 
-  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
